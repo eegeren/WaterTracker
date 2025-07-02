885ca704b570c8e86c75f2fb9909dc09471783c7
Yusufege Eren <yusufege.erenn@gmail.com>
2025-07-02 13:32:02 +0300

    Initial Commit

---
+++ WaterReminder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WaterReminder
+//
+//  Created by Yusufege Eren on 2.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WaterReminderTests/WaterReminderTests.swift
@@ -0,0 +1,17 @@
+//
+//  WaterReminderTests.swift
+//  WaterReminderTests
+//
+//  Created by Yusufege Eren on 2.07.2025.
+//
+
+import Testing
+@testable import WaterReminder
+
+struct WaterReminderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
